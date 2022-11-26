e56ad5cfd039bec1f56e9fa72164f0341bf18ff3
nikito <aa@aa.aa>
2022-11-26 11:43:04 +0200

    out third commit where created a new sout

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Java pro the best group in hillel");
+
+        System.out.println("Created new sout for test");
     }
 }
